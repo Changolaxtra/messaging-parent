964d160623bd21d238ba048049b85285c6d0ce12
Daniel Rojas <v-darojas@expediagroup.com>
2022-06-14 15:40:14 -0500

    first commit

---
+++ event-service-api/src/main/java/dan/rojas/epam/api/App.java
@@ -0,0 +1,13 @@
+package dan.rojas.epam.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
