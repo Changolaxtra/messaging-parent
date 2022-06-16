3e4be529c8d0a2a29aee39f653d8c2fe0e76e45c
Daniel Rojas <v-darojas@expediagroup.com>
2022-06-16 12:05:23 -0500

    module added

---
+++ event-messaging/src/main/java/dan/rojas/epam/messaging/EventMessagingApplication.java
@@ -0,0 +1,13 @@
+package dan.rojas.epam.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMessagingApplication.class, args);
+	}
+
+}
