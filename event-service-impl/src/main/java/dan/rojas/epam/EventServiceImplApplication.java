bafd4781a9ca323ac5c7c0229368c46f46fc4406
Daniel Rojas <dan.rojasx@gmail.com>
2022-06-15 21:16:21 -0500

    dependencies fixed

---
+++ event-service-impl/src/main/java/dan/rojas/epam/EventServiceImplApplication.java
@@ -0,0 +1,11 @@
+package dan.rojas.epam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventServiceImplApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventServiceImplApplication.class, args);
+    }
+}
--- event-service-rest/src/main/java/dan/rojas/epam/rest/controller/EventServiceController.java
+++ event-service-rest/src/main/java/dan/rojas/epam/rest/controller/EventServiceController.java
@@ -2,8 +2,6 @@ package dan.rojas.epam.rest.controller;
 
 import dan.rojas.epam.dto.Event;
 import dan.rojas.epam.service.EventServiceDb;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,34 +17,32 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/events")
-@Api(value = "Swagger2DemoRestController", description = "REST APIs related to Student Entity!!!!")
 public class EventServiceController {
 
-  private final EventServiceDb eventServiceDb;
+    private final EventServiceDb eventServiceDb;
 
-  @GetMapping
-  @ApiOperation(value = "Get list of Students in the System ", response = Iterable.class, tags = "getStudents")
-  public List<Event> getAllEvents() {
-    return eventServiceDb.getAllEvents();
-  }
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventServiceDb.getAllEvents();
+    }
 
-  @GetMapping("/{id}")
-  public Event getEvent(@PathVariable final Long id) {
-    return eventServiceDb.getEvent(id);
-  }
+    @GetMapping("/{id}")
+    public Event getEvent(@PathVariable final Long id) {
+        return eventServiceDb.getEvent(id);
+    }
 
-  @DeleteMapping("/{id}")
-  public Event deleteEvent(@PathVariable final Long id) {
-    return eventServiceDb.deleteEvent(id);
-  }
+    @DeleteMapping("/{id}")
+    public Event deleteEvent(@PathVariable final Long id) {
+        return eventServiceDb.deleteEvent(id);
+    }
 
-  @PostMapping
-  public Event saveEvent(@RequestBody final Event event) {
-    return eventServiceDb.createEvent(event);
-  }
+    @PostMapping
+    public Event saveEvent(@RequestBody final Event event) {
+        return eventServiceDb.createEvent(event);
+    }
 
-  @PutMapping
-  public Event updateEvent(@RequestBody final Event event) {
-    return eventServiceDb.updateEvent(event.getEventId(), event);
-  }
+    @PutMapping
+    public Event updateEvent(@RequestBody final Event event) {
+        return eventServiceDb.updateEvent(event.getEventId(), event);
+    }
 }
